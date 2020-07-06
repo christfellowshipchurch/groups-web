020f35f485b2882f30392b51a6a86b3642b72826
Burke Shartsis <burke@differential.com>
2020-07-06 12:06:53 -0400

    add dotenv support

--- src/pages/boom/index.js
+++ src/pages/boom/index.js
@@ -3,7 +3,12 @@ import styled from "styled-components";
 import { Title } from "../../ui";
 
 function Boom() {
-  return <Title>Boom page</Title>;
+  return (
+    <>
+      <Title>Boom page</Title>
+      <p>Environment variable process.env.TEST is "{process.env.TEST}"</p>
+    </>
+  );
 }
 
 export default Boom;
