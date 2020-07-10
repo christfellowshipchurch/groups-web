930e48c5960462922406976b1e302aa1ba8748e5
Burke Shartsis <burke@differential.com>
2020-07-10 14:25:48 -0400

    the start of some theme values.
    - these were just what was needed for CardWrapper. I expect things to 
    change.

--- src/ui-kit/theme/defaultTheme.js
+++ src/ui-kit/theme/defaultTheme.js
@@ -5,18 +5,18 @@ const theme = {
       paper: '#FFFFFF',
     },
   },
-
-  // Base sizing units. These are used to scale space, and size components relatively to one another.
   sizing: {
-    baseUnit: '16px',
-    baseBorderRadius: '16px',
+    baseUnit: '16',
+    baseBorderRadius: '16',
     avatar: {
       small: '40',
       medium: '80',
       large: '120',
     },
   },
-  shadows: { default: 'box-shadow: 0px 5px 18px rgba(0, 0, 0, 0.1)' },
+  shadows: {
+    default: 'box-shadow: 0px 5px 18px rgba(0, 0, 0, 0.1)',
+  },
 };
 
 export default theme;
