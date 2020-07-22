4ee84a7192e0e16b9edf9aa47f2e99c0be5da304
Burke Shartsis <burke@differential.com>
2020-07-22 17:18:10 -0400

    cleanup PaddedDiv story

--- src/ui-kit/PaddedDiv/PaddedDiv.stories.js
+++ src/ui-kit/PaddedDiv/PaddedDiv.stories.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react';
 import PaddedDiv from '.';
 
 storiesOf('ui-kit/PaddedDiv', module)
+  .addDecorator((story) => (
+    <div style={{ width: '50%', background: 'salmon' }}>{story()}</div>
+  ))
   .add('default', () => <PaddedDiv>This text is in a PaddedDiv</PaddedDiv>)
   .add('horizontal (false)', () => (
     <PaddedDiv horizontal={false}>
