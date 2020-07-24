8e4477e676fa99659d6d027be84894a8aec28dbd
Burke Shartsis <burke@differential.com>
2020-07-23 22:24:15 -0400

    fix Media placeholder story

--- src/ui-kit/Placeholder/Media/Media.stories.js
+++ src/ui-kit/Placeholder/Media/Media.stories.js
@@ -6,6 +6,9 @@ import 'react-placeholder/lib/reactPlaceholder.css';
 import Media from '.';
 
 storiesOf('ui-kit/Placeholder/Media', module)
+  .addDecorator((story) => (
+    <div style={{ width: '50vw', height: '50vw' }}>{story()}</div>
+  ))
   .add('default', () => <Media />)
   .add('width', () => <Media size={'50%'} />)
   .add('showLoadingAnimation', () => <Media showLoadingAnimation />);
