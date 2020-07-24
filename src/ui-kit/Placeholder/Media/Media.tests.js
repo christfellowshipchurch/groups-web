9d9d8ca25df85b852418f0a619ab1d3f47e2b363
Burke Shartsis <burke@differential.com>
2020-07-23 23:32:39 -0400

    more clean up

--- src/ui-kit/Placeholder/Media/Media.tests.js
+++ src/ui-kit/Placeholder/Media/Media.tests.js
@@ -6,7 +6,7 @@ import Providers from '../Providers';
 import { Media } from './Media';
 
 describe('The Media placeholder', () => {
-  it('renders correctly', () => {
+  it('should render', () => {
     const tree = renderer.create(
       <Providers>
         <Media />
@@ -14,20 +14,10 @@ describe('The Media placeholder', () => {
     );
     expect(tree).toMatchSnapshot();
   });
-  it('accepts size', () => {
+  it('should render a custom size', () => {
     const tree = renderer.create(
       <Providers>
         <Media size="50%" />
-      </Providers>
-    );
-    expect(tree).toMatchSnapshot();
-  });
-  it('accepts custom styles', () => {
-    const customStyle = { backgroundColor: 'salmon' };
-
-    const tree = renderer.create(
-      <Providers>
-        <Media style={customStyle} />
       </Providers>
     );
     expect(tree).toMatchSnapshot();
