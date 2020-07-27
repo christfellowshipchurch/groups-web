638716ddecdfcf2be328a0a0736b572eef9aac8b
Burke Shartsis <burke@differential.com>
2020-07-27 17:25:25 -0400

    fix placeholder animations
    - this also fixed jest

--- src/ui-kit/theme/GlobalStyles/index.js
+++ src/ui-kit/theme/GlobalStyles/index.js
@@ -1,8 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
-import * as defaultPlaceholderAnimationStyle from 'react-placeholder/lib/reactPlaceholder.css';
 
 const GlobalStyles = createGlobalStyle`
-  ${defaultPlaceholderAnimationStyle}
+  /* Copied from https://github.com/buildo/react-placeholder/blob/master/src/reactPlaceholder.scss
+  to reduce technical complexity with importing css */
+  .show-loading-animation {
+    &.rect-shape,
+    &.round-shape,
+    &.text-row,
+    &.custom-placeholder, /* We add a custom class for use in our placeholder components */
+    .rect-shape,
+    .round-shape,
+    .text-row,
+    .custom-placeholder { /* We add a custom class for use in our placeholder components */
+      animation: react-placeholder-pulse 1.5s infinite;
+    }
+  }
+
+  @keyframes react-placeholder-pulse {
+    0% {
+      opacity: .6;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: .6;
+    }
+  }
 `;
 
 export default GlobalStyles;
