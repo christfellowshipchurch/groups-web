df31baf987384d4055d8c87c6010b318c518f623
Burke Shartsis <burke@differential.com>
2020-07-28 22:29:22 -0400

    add theme typography section and verticalRhythm helper

--- src/ui-kit/theme/defaultTheme.js
+++ src/ui-kit/theme/defaultTheme.js
@@ -1,10 +1,19 @@
 const theme = {
   colors: {
     primary: 'salmon',
+    text: {
+      primary: '#303030',
+    },
     background: {
       paper: '#FFFFFF',
       inactive: '#A5A5A5',
     },
+  },
+  typography: {
+    baseFontSize: 1,
+    baseLineHeight: 1.5,
+    fontFamily:
+      'Gotham HTF, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Arial, sans-serif',
   },
   sizing: {
     baseUnit: (unit = 1) => `${unit}rem`,
@@ -17,6 +26,23 @@ const theme = {
   },
   shadows: {
     default: '0px 5px 18px rgba(0, 0, 0, 0.1)',
+  },
+  helpers: {
+    /*
+     * Vertical rhythm method of spacing objects (typically typographic elements) based on a predefined
+     * ratio. A popular ratio that is often used would be something like the Golden Ratio. With the
+     * Golden Ratio as an example, all elment spacing on the page is a multiplied derivitive of that
+     * ratio resulting in a rhthmic spacing. This is often used in typography to set line heights and
+     * vertical paddings. The helper function below takes a `rem` unit (often a font size but not always)
+     * and multiplies it by either a custom ratio or a derrived ratio from the themes `baseLineHeight`
+     * devided by the `baseFontSize`.
+     */
+    verticalRhythm: (units, customRatio) => {
+      const verticalRatio =
+        customRatio ||
+        theme.typography.baseLineHeight / theme.typography.baseFontSize;
+      return `${verticalRatio * units}rem`;
+    },
   },
 };
 
