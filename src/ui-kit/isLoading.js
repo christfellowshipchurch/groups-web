d9c560db1cf37ad7d8488ff07eee8c1c37ec430c
Burke Shartsis <burke@differential.com>
2020-07-31 21:48:12 -0400

    port and rewrite isLoading from Apollos to not use recompose.

---
+++ src/ui-kit/isLoading.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+
+// Create our IsLoadingContext
+const IsLoadingContext = React.createContext(false);
+
+/* Safely checks for isLoading to make it easy to build components that respond to either props or
+ * context for rendering placeholder/loading state. */
+const getIsLoading = (Component) => (props) => {
+  // does the component have an isLoading property?
+  if (Object.hasOwnProperty.call(props, 'isLoading')) {
+    return <Component {...props} />;
+  }
+
+  // No? That's fine we'll pull it from context
+  const isLoading = useContext(IsLoadingContext);
+
+  return <Component isLoading={isLoading} {...props} />;
+};
+
+// Allows a component to accept an `isLoading` prop to pass down into context for it's children.
+const withIsLoading = (Component) => ({ isLoading, props }) => {
+  return (
+    <IsLoadingContext.Provider value={isLoading}>
+      <Component {...props} />
+    </IsLoadingContext.Provider>
+  );
+};
+
+export { getIsLoading, IsLoadingContext, withIsLoading };
