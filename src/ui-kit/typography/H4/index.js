80afaad70b39431fbb44f9cfb76555fc3f302f4a
Burke Shartsis <burke@differential.com>
2020-08-05 12:48:03 -0400

    add theme override support to H4

--- src/ui-kit/typography/H4/index.js
+++ src/ui-kit/typography/H4/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
+import { withOverrides } from '../../theme';
 import { Line } from '../../Placeholder';
 import { getIsLoading } from '../../isLoading';
 
@@ -14,21 +15,19 @@ const verticalRhythm = css`
       : 'initial'};
 `;
 
-const StyledH4 = styled.h4.attrs({
-  className: 'ui-kit-typography-H4-StyledH4',
-})`
+const StyledH4 = styled.h4.attrs(withOverrides('uiKit.typography.H4.StyledH4'))`
   font-size: 1rem;
   font-weight: 700;
   line-height: ${({ theme }) => theme.helpers.verticalRhythm(1)};
   margin: ${verticalRhythm};
 
-  /* Styles passed via the style prop */
-  ${({ customStyles }) => customStyles}
+  /* Styles passed via the style prop and/or overrides */
+  ${({ $style }) => $style}
 `;
 
-const Placeholder = styled(Line).attrs({
-  className: 'ui-kit-H4-Placeholder',
-})`
+const Placeholder = styled(Line).attrs(
+  withOverrides('uiKit.typography.H4.Placeholder')
+)`
   height: 1rem; /* Should match H4 font-size */
   margin: ${({ withMargins, theme }) =>
     // if withMargins...
@@ -39,6 +38,9 @@ const Placeholder = styled(Line).attrs({
         `calc(${theme.helpers.verticalRhythm(
           1
         )} - 1rem) 0`}; /* line-height - font-size = space between lines */
+
+  /* Styles passed via overrides */
+  ${({ $style }) => $style}
 `;
 
 // We pass `style` as a prop to `StyledH4` so that custom styles are handled by styled components and not react (inline style)
@@ -49,7 +51,7 @@ const H4 = getIsLoading(
       withMargins={withMargins}
       showLoadingAnimation
     >
-      <StyledH4 withMargins={withMargins} customStyles={style} {...props}>
+      <StyledH4 withMargins={withMargins} $styles={style} {...props}>
         {children}
       </StyledH4>
     </Placeholder>
@@ -65,4 +67,4 @@ H4.propTypes = {
 
 H4.displayName = 'ui-kit.typography.H4';
 
-export default H4;
+export default styled(H4).attrs(withOverrides('uiKit.typography.H4'))``;
