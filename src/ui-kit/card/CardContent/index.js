9c9b0c496e0b324408b2d8df4d8fb258458f4516
Burke Shartsis <burke@differential.com>
2020-08-06 15:59:45 -0400

    CardContent paddings were reversed

--- src/ui-kit/card/CardContent/index.js
+++ src/ui-kit/card/CardContent/index.js
@@ -7,7 +7,7 @@ const CardContent = styled(PaddedDiv).attrs(
   withOverrides('ui-kit.card.CardContent')
 )`
   padding: ${({ theme }) =>
-    `${theme.sizing.baseUnit(1.5)} ${theme.sizing.baseUnit(2)}`};
+    `${theme.sizing.baseUnit(2)} ${theme.sizing.baseUnit(1.5)}`};
 `;
 
 CardContent.displayName = 'ui-kit.card.CardContent';
