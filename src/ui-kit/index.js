1b4041c43a54f8d719924702fc2d22342def04b6
Burke Shartsis <burke@differential.com>
2020-08-06 14:38:00 -0400

    add ui-kit exports

--- src/ui-kit/index.js
+++ src/ui-kit/index.js
@@ -1,1 +1,6 @@
+export { CardWrapper, CardContent } from './card';
+export { getIsLoading, IsLoadingContext, withIsLoading } from './isLoading';
+export PaddedDiv from './PaddedDiv';
+export Placeholder, { Line, Media, Paragraph } from './Placeholder';
+export { withOverrides } from './theme';
 export { H1, H2, H3, H4, H5, H6 } from './typography';
