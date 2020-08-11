06b5140f6645d215e5d6488665abfba3a8d8c5f1
Burke Shartsis <burke@differential.com>
2020-08-11 13:54:08 -0400

    fix storybook not liking svg imports
    - bumps storybook and nextjs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,17 @@
 module.exports = {
-  stories: ["../src/**/*.stories.[tj]s"]
+  stories: ["../src/**/*.stories.[tj]s"],
+  webpackFinal: async (config, { configType }) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => !Array.isArray(rule.test) && rule.test.test('.svg'),
+    );
+
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 };
