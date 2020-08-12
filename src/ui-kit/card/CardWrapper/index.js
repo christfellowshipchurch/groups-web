b29aa5871dc32ae06e09be2fadf8ac64cc0319d6
Burke Shartsis <burke@differential.com>
2020-08-12 13:17:42 -0400

    add withIsLoading to CardWrapper

--- src/ui-kit/card/CardWrapper/index.js
+++ src/ui-kit/card/CardWrapper/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import { withOverrides } from '../../theme';
+import { withIsLoading } from '../../isLoading';
 
 const CardWrapper = styled.div.attrs(withOverrides('ui-kit.card.CardWrapper'))`
   /* card styles */
@@ -10,20 +11,21 @@ const CardWrapper = styled.div.attrs(withOverrides('ui-kit.card.CardWrapper'))`
     theme.colors.background.paper ||
     `initial`}; /* bail out if no bg color */
   border-radius: ${({ theme }) => theme.sizing.baseBorderRadius};
+  box-shadow: ${({ theme }) => theme.shadows.default};
   margin: ${({ inHorizontalList, theme }) =>
     inHorizontalList
       ? `${theme.sizing.baseUnit(0.5)}
       ${theme.sizing.baseUnit(0.5)}
       ${theme.sizing.baseUnit(0.75)}`
       : `${theme.sizing.baseUnit(0.75)} ${theme.sizing.baseUnit(1)}`};
-  box-shadow: ${({ theme }) => theme.shadows.default};
 `;
 
 CardWrapper.propTypes = {
   cardColor: PropTypes.string,
   children: PropTypes.node.isRequired,
+  isLoading: PropTypes.bool,
 };
 
 CardWrapper.displayName = 'ui-kit.card.CardWrapper';
 
-export default CardWrapper;
+export default withIsLoading(CardWrapper);
