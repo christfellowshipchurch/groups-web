1a4fc8faa49f725d81d5e032b55baf77507eecaa
Burke Shartsis <burke@differential.com>
2020-08-12 10:43:54 -0400

    add tests over withIsLoading to catch my mistakes
    Co-Authored-By: @conrad-vanl <conrad@differential.com>

--- src/ui-kit/isLoading/withIsLoading.tests.js
+++ src/ui-kit/isLoading/withIsLoading.tests.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-// import Providers from '../../Providers';
-
 import { getIsLoading, withIsLoading } from '.';
 
 describe('the withIsLoading HOC', () => {
+  it('should render', () => {
+    const ParentComponentPassingConext = withIsLoading(
+      ({ children }) => children
+    );
+
+    const tree = renderer.create(
+      <ParentComponentPassingConext>
+        <h1>Boom</h1>
+      </ParentComponentPassingConext>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
   it('should pass the isLoading prop (true) to context for children to consume', () => {
     const ChildComponentGettingContext = getIsLoading(({ isLoading }) =>
       isLoading.toString()
